bf29c5e664ca5ea9dab8aa8931c0e5320957d4a2
Andreas Bichinger <andreas.bichinger@gmail.com>
2021-12-03 16:10:09 +0100

    ci: add semantic release

---
+++ scripts/gen_rmskin.js
@@ -0,0 +1,38 @@
+const jspack = require('jspack').jspack;
+const chr = (n) => String.fromCharCode(n)
+const fs = require('fs')
+const archiver = require('archiver')
+
+let rmskinPath = process.argv[2]
+
+//create zip
+let rmskin = archiver('zip')
+let rmskinOutput = fs.createWriteStream(rmskinPath)
+
+rmskinOutput.on('close', function() {
+    console.log(rmskin.pointer() + ' total bytes');
+
+    //prepare footer
+    let rmskinStats = fs.statSync(rmskinPath)
+
+    let key = 'RMSKIN\x00'
+    let fileSize = rmskinStats.size
+    let flags = chr(0)
+
+    console.log('file size:',fileSize)
+
+    let bytes = jspack.Pack('<lxxxxB7s', [fileSize, flags, key])
+    let footer = Buffer.from(bytes)
+    console.log(footer.length, footer)
+
+    fs.appendFileSync(rmskinPath, footer)
+});
+
+rmskin.pipe(rmskinOutput)
+
+rmskin.file('./RMSKIN.ini', {name: 'RMSKIN.ini'})
+rmskin.directory('./Skins/', 'Skins/OHM')
+rmskin.file('build/x32/Release/OpenHardwareMonitor.dll', {name: 'Plugins/32bit/OpenHardwareMonitor.dll'})
+rmskin.file('build/x64/Release/OpenHardwareMonitor.dll', {name: 'Plugins/64bit/OpenHardwareMonitor.dll'})
+
+rmskin.finalize()
---
+++ scripts/update_version.js
@@ -0,0 +1,23 @@
+const ini = require('ini')
+const fs = require('fs')
+
+nextRelease = process.argv[2]
+rmskinINI = './RMSKIN.ini'
+asseblyInfo = "./OpenHardwareMonitor/AssemblyInfo.cs"
+csprojPath = "./OpenHardwareMonitor/OpenHardwareMonitor.csproj"
+
+// update RMSKIN.ini version
+var config = ini.parse(fs.readFileSync(rmskinINI, 'utf-8'))
+config.rmskin.Version = nextRelease
+fs.writeFileSync(rmskinINI, ini.stringify(config))
+
+// update AsseblyInfo.cs
+var info = fs.readFileSync(asseblyInfo, 'utf-8')
+info = info.replace(/AssemblyVersion\(".*"\)/gi, `AssemblyVersion("${nextRelease}")`)
+fs.writeFileSync(asseblyInfo, info)
+
+// update OpenHardwareMonitor.csproj
+var csproj = fs.readFileSync(csprojPath, 'utf-8')
+csproj = csproj.replace(/<ApplicationVersion>.*<\/ApplicationVersion>/gi, `<ApplicationVersion>${nextRelease}</ApplicationVersion>`)
+fs.writeFileSync(csprojPath, csproj)
+
